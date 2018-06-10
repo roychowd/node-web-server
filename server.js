2a3258056b7b99647423ac463810336d4ac71d85
Roy Chowdhury <chowdhury.roy4@gmail.com>
2018-06-10 11:47:27 -0600

    modifying for heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 
@@ -58,7 +60,7 @@ app.get('/bad', (req,res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("server is up on port 3000");
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
     
 }); 
